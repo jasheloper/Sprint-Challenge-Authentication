63145ebfa45634cbdd9a19a0abe7b0557fd09453
Jashele Tillman <48870048+jasheloper@users.noreply.github.com>
2019-12-15 13:01:43 -0700

    /auth/auth-router + /register and /login working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,32 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
 
-router.post('/register', (req, res) => {
+const Users = require('../users/users-model.js');
+
+
+
+router.post("/register", (req, res) => {
   // implement registration
+
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password);
+  user.password = hash;
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
-router.post('/login', (req, res) => {
+
+router.post("/login", (req, res) => {
   // implement login
+
+  let { username } = req.headers;
+  res.status(200).json({ message: `Welcome ${username}!` });
 });
+
 
 module.exports = router;
--- notes.js
+++ notes.js
@@ -1,5 +1,50 @@
-// Notes:
+/* 
 
-// - Forked.
+-------------------------------------
 
-// Read Me - Self-Study / Essay Questions 
+Notes on my process, just for fun! 
+
+-------------------------------------
+
+
+- Forked.
+- Read Me - Self-Study / Essay Questions  
+
+
+Installing: 
+- yarn add express
+- yarn
+- yarn server | Server Listens
+
+
+-------------------------------------
+Implement User Authentication System:
+
+/ A 1 way process where we take a user password in plain text & translate it into a string that you can no longer reverse back into the password. /
+
+
+- npm install bcryptjs
+
+1. Implement the register and login functionality inside /auth/auth-router.js.
+   - require bcrypt 
+   - fill in /register and /login 
+   - create a users model (useres > users-model.js) to require in auth-router
+
+   - POSTMAN   : localhost:3300/api/auth/register
+      - Test user register : SUCCESS 
+      
+   - POSTMAN   : localhost:3300/api/auth/login
+      - Test User Login    : SUCCESS
+
+
+
+
+-------------------------------------
+
+
+
+
+
+
+
+*/
